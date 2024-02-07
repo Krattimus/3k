9c99620f367c9af3b97dd4e2df886cfffff14cd5
Krat <republokrat@gmail.com>
2024-02-07 11:07:00 -0800

    latest

--- TinTin/Tools/aiImageGenerator.py
+++ TinTin/Tools/aiImageGenerator.py
@@ -2,19 +2,24 @@ import requests
 import json
 import webbrowser
 import sys
-from tintin import TinTin
+
 url = "https://api.openai.com/v1/images/generations"
 
 payload = json.dumps({
-  "prompt": sys.argv[1]
+  "prompt": sys.argv[1],
+  "model": "dall-e-3",
+  "size": "1024x1024",
+  "quality": "hd",
+  "n": 1
 })
+
 headers = {
   'Authorization': 'Bearer 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',
   'Content-Type': 'application/json',
-  'Cookie': '__cf_bm=5keMMMocu7nmnRclBj7_NhPZwrf573LVJ8hoQRIU1W4-1704442402-1-ATS5+C1tXrWxA3ZlKhg5WTEc0fDKeylWjIK19O6TgahyaBaD+sgMa4LGhNqZa+KkG6nh8GcXKpmP6aYNC0wm2Ag=; _cfuvid=TZgb9Ydz7s6yky9G5BsylQejpEZ4ll1sBD_CLXQDlUE-1704442402745-0-604800000'
 }
 
 response = requests.request("POST", url, headers=headers, data=payload)
 json = response.json()
-
 webbrowser.open(json["data"][0]["url"], new=0, autoraise=True)
+
+
--- TinTin/Tools/byronAPI.sh
+++ TinTin/Tools/byronAPI.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-curl -X POST https://www.3kpi.icu/api/area/ -H "Content-Type: application/json" -d "{\"name\" : \"$1\", \"dungeon\" : \"false\", \"room_count\" : $2, \"wizard\" : \"$3\"}"
+#curl -X POST https://www.3kpi.icu/api/area/ -H "Content-Type: application/json" -d "{\"name\" : \"$1\", \"dungeon\" : \"false\", \"room_count\" : $2, \"wizard\" : \"$3\"}"
+
+curl --location "https://api.openai.com/v1/images/generations" \
+--header "Authorization: Bearer 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" \
+--header "Content-Type: application/json" \
+--header "Cookie: __cf_bm=5keMMMocu7nmnRclBj7_NhPZwrf573LVJ8hoQRIU1W4-1704442402-1-ATS5+C1tXrWxA3ZlKhg5WTEc0fDKeylWjIK19O6TgahyaBaD+sgMa4LGhNqZa+KkG6nh8GcXKpmP6aYNC0wm2Ag=; _cfuvid=TZgb9Ydz7s6yky9G5BsylQejpEZ4ll1sBD_CLXQDlUE-1704442402745-0-604800000" \
+--data "{
+    \"prompt\" : \"$1\"
+}"
